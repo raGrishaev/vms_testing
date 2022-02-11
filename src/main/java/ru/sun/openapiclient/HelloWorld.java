1f72cf857e3cfcb3bc3419cb7ada9516259b3016
raGrishaev <ro.grishaev@gmail.com>
2022-02-12 00:23:25 +0300

    add dependencies testng

--- src/main/java/ru/sun/openapiclient/HelloWorld.java
+++ src/main/java/ru/sun/openapiclient/HelloWorld.java
@@ -3,5 +3,7 @@ package ru.sun.openapiclient;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Second string");
+        System.out.println("Third string");
     }
 }
